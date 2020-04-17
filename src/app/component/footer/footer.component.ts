52b15953a13f5795740c3da55a1883e996f52f00
Cristhian.Eduardo.Cortes.Martha <cristhianeduardocortes@gmail.com>
2020-04-16 23:42:12 -0500

    Se alinia el texto de footer de manera central en todas las paginas :)

--- src/app/component/footer/footer.component.ts
+++ src/app/component/footer/footer.component.ts
@@ -1,7 +1,6 @@
 import { Component } from '@angular/core';
 
-@Component({  
-  moduleId: module.id,
+@Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
