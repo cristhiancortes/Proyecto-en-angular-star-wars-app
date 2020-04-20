a4a78f04d3a6f803a18cff7cedb414bfed840761
Jeisson <jegomezh2@poligran.edu.co>
2020-04-20 18:23:33 -0500

    personas

--- src/app/component/personas/personas.component.ts
+++ src/app/component/personas/personas.component.ts
@@ -6,5 +6,15 @@ import { Component } from '@angular/core';
   styleUrls: ['./personas.component.css']
 })
 export class PersonasComponent {
-
+  public titulo = 'personas'
+  public nombre = './personas.component.html';
+  public altura = './personas.component.html';
+  public masa = './personas.component.html';
+  public color_de_pelo = './personas.component.html';
+  public color_de_piel = './personas.component.html';
+  public color_de_los_ojos = './personas.component.html';
+  public año_de_nacimiento = './personas.component.html';
+  public genero = './personas.component.html';
+  public mundo_natal = './personas.component.html';
+  public peliculas = './personas.component.html';
 }
